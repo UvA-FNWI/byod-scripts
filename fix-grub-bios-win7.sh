b9cab5c650a7cba338a831698545645e26dfd557
S.J.R. van Schaik <stephan@synkhronix.com>
2014-08-25 20:26:01 +0100

    shell script om GRUB BIOS te fixen voor Microsoft Windows 7 en later.

---
+++ fix-grub-bios-win7.sh
@@ -0,0 +1,31 @@
+if test -z $SYS_RESERVED; then
+export SYS_RESERVED=$(blkid | grep -m1 'LABEL="SYSTEM RESERVED"' | cut -d
+':' -f1);
+fi
+
+if test -z $SYS_RESERVED; then
+export SYS_RESERVED=$(blkid | grep -m1 'LABEL="SYSTEM"' | cut -d ':' -f1);
+fi
+
+if test -z $SYS_RESERVED; then
+echo "error: system reserved not found.";
+exit 1
+fi
+
+mkdir -p /media/SYSTEM_RESERVED
+mount $SYS_RESERVED /media/SYSTEM_RESERVED
+cat << __EOF__ >> /etc/grub.d/40_custom
+if [ "${grub_platform}" == "pc" ]; then
+	menuentry "Microsoft Windows Vista/7/8/8.1" {
+		insmod part_msdos
+		insmod ntfs
+		insmod search_fs_uuid
+		insmod ntldr
+		search --fs-uuid --set=root $(grub-probe --target=hints_string /media/SYSTEM_RESERVED/bootmgr) $(grub-probe --target=fs_uuid /media/SYSTEM_RESERVED/bootmgr)
+		ntldr /bootmgr
+	}
+fi
+__EOF__
+umount /media/SYSTEM_RESERVED
+rmdir /media/SYSTEM_RESERVED
+

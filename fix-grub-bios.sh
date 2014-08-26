d838c3868db957e9f6725e78cc0b14852884de0a
S.J.R. van Schaik <stephan@synkhronix.com>
2014-08-26 21:34:18 +0100

    ELOG should work now.

--- fix-grub-bios.sh
+++ fix-grub-bios.sh
@@ -5,11 +5,11 @@ fi
 
 # Attempt to find the system reserved partition.
 if test -z $SYS_RESERVED; then
-export SYS_RESERVED=$(blkid 2>$ELOG | grep -m1 'LABEL="SYSTEM RESERVED"' 2>$ELOG | cut -d ':' -f1 2>$ELOG)
+export SYS_RESERVED=$(blkid 2>>$ELOG | grep -m1 'LABEL="SYSTEM RESERVED"' 2>>$ELOG | cut -d ':' -f1 2>>$ELOG)
 fi
 
 if test -z $SYS_RESERVED; then
-export SYS_RESERVED=$(blkid 2>$ELOG | grep -m1 'LABEL="SYSTEM"' 2>$ELOG | cut -d ':' -f1 2>$ELOG)
+export SYS_RESERVED=$(blkid 2>>$ELOG | grep -m1 'LABEL="SYSTEM"' 2>>$ELOG | cut -d ':' -f1 2>>$ELOG)
 fi
 
 # Prompt the user for the system reserved partition.
@@ -25,23 +25,23 @@ exit 1
 fi
 
 # Attempt to mount the partition.
-mkdir -p /media/SYSTEM_RESERVED 2>$ELOG
-mount $SYS_RESERVED /media/SYSTEM_RESERVED 2>$ELOG
+mkdir -p /media/SYSTEM_RESERVED 2>>$ELOG
+mount $SYS_RESERVED /media/SYSTEM_RESERVED 2>>$ELOG
 if [ $? -ne 0 ]; then
 echo "error: unable to mount $SYS_RESERVED."
-rmdir /media/SYSTEM_RESERVED 2>$ELOG
+rmdir /media/SYSTEM_RESERVED 2>>$ELOG
 exit 1
 fi
 
 # Ask GRUB for some necessary details to put into the menu entry.
-export HINTS_STRING=$(grub-probe --target=hints_string /media/SYSTEM_RESERVED/bootmgr 2>$ELOG)
-export FS_UUID=$(grub-probe --target=fs_uuid /media/SYSTEM_RESERVED/bootmgr 2>$ELOG)
+export HINTS_STRING=$(grub-probe --target=hints_string /media/SYSTEM_RESERVED/bootmgr 2>>$ELOG)
+export FS_UUID=$(grub-probe --target=fs_uuid /media/SYSTEM_RESERVED/bootmgr 2>>$ELOG)
 
 # Make sure we have got those details.
 if test -z "$HINTS_STRING" || test -z "$FS_UUID"; then
 echo "error: is grub-probe installed?";
-umount /media/SYSTEM_RESERVED 2>$ELOG
-rmdir /media/SYSTEM_RESERVED 2>$ELOG
+umount /media/SYSTEM_RESERVED 2>>$ELOG
+rmdir /media/SYSTEM_RESERVED 2>>$ELOG
 exit 1
 fi
 
@@ -58,6 +58,6 @@ menuentry "Microsoft Windows Vista/7/8/8.1" {
 __EOF__
 
 # Clean up.
-umount /media/SYSTEM_RESERVED 2>$ELOG
-rmdir /media/SYSTEM_RESERVED 2>$ELOG
+umount /media/SYSTEM_RESERVED 2>>$ELOG
+rmdir /media/SYSTEM_RESERVED 2>>$ELOG
 

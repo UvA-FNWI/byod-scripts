f187336a0869054a625c5526a3ecd80b2f6ac07f
Robin de Vries <robin@celp.nl>
2016-07-24 21:59:41 +0200

    Update script to 16.04

--- install-extras.sh
+++ install-extras.sh
@@ -15,56 +15,56 @@ if [[ $SUDO_UID -eq 0 ]]; then
    exit 1
 fi
 
-
-if ! `lsb_release -c | grep -q trusty`; then
-    echo "This script can only be executed on a machine running Ubuntu 14.04 LTS"
+if ! `lsb_release -c | grep -q xenial`; then
+    echo "This script can only be executed on a machine running Ubuntu 16.04 LTS"
     exit 1
 fi
 
 echo "University of Amsterdam - E.M. Kooistra - S.J.R. van Schaik - R. de Vries"
 
 # Install gnome-flashback-session
-echo "[1/9] Installing gnome-flashback-session"
+echo "[0/9] Installing gnome-flashback-session"
 apt-get -y install gnome-session-flashback  &>> install_extras_log
 gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Alt>F2']"
 gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close
 
 # Set gnome-session-flashback as default
-echo "[2/9] Setting gnome-session-flashback as default"
-sed -i 's/user-session=ubuntu/user-session=gnome-fallback/g' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
-sed -i 's/XSession=ubuntu/Xsession=gnome-fallback/g' /var/lib/AccountsService/users/$SUDO_USER
+echo "[1/9] Setting gnome-session-flashback as default"
+sed -i 's/user-session=ubuntu/user-session=gnome-flashback-compiz/g' /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
+sed -i 's/XSession=ubuntu/Xsession=gnome-flashback-compiz/g' /var/lib/AccountsService/users/$SUDO_USER
 
 # Install oracle java and UvAvpn
-echo "[3/9] Installing Java / UvA-VPN"
+echo "[2/9] Installing Java / UvA-VPN / Atom"
 add-apt-repository -y ppa:webupd8team/java &>> install_extras_log
+add-apt-repository -y ppa:webupd8team/atom &>> install_extras_log
 add-apt-repository -y ppa:uva-informatica/uvavpn &>> install_extras_log
 apt-get -y update &>> install_extras_log
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get -y install oracle-java8-installer oracle-java8-set-default uvavpn &>> install_extras_log
+apt-get -y install oracle-java8-installer oracle-java8-set-default uvavpn atom &>> install_extras_log
 
 # Install development tools
-echo "[4/9] Installing development tools"
+echo "[3/9] Installing development tools"
 apt-get -y install build-essential git vim valgrind flex bison gnuplot gnuplot-qt graphviz &>> install_extras_log
 
 # Programming languages for the programming-languages course
-echo "[5/9] Installing Prolog, Erlang, Go and Haskell"
+echo "[4/9] Installing Prolog, Erlang, Go and Haskell"
 apt-get -y install swi-prolog erlang ghc golang haskell-platform &>> install_extras_log
 
 # Install several python packages
-echo "[6/9] Installing several Python packages"
+echo "[5/9] Installing several Python packages"
 apt-get -y install python-scipy python-numpy python-matplotlib python3-scipy python3-numpy python3-matplotlib &>> install_extras_log
 
 # Install Chromium
-echo "[7/9] Installing Chromium webbrowser"
+echo "[6/9] Installing Chromium webbrowser"
 apt-get -y install chromium-browser &>> install_extras_log
 
 # Install LaTeX
-echo "[8/9] Installing LaTeX (this can take up to 45 minutes)"
+echo "[7/9] Installing LaTeX (this can take up to 45 minutes)"
 apt-get -y install texlive-full &>> install_extras_log
 
 #check for updates
-echo "[9/9] Updating packages"
+echo "[8/9] Updating packages"
 apt-get -y upgrade &>> install_extras_log
 
 # Finish and reboot!
-echo "Reboot your computer now"
+echo "[9/9] Finished, reboot your computer now"

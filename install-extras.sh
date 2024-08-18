de2b93713722e8b84e1393fd98caf15de998b9b5
Sam van Kampen <sam@tehsvk.net>
2024-08-19 01:52:07 +0200

    also install dbus-x11 in code install path

--- install-extras.sh
+++ install-extras.sh
@@ -152,6 +152,7 @@ function install_code {
     if dpkg -l code; then
         echo "Skipping, Visual Studio Code already installed"
     else
+        apt-get $APT_OPTIONS install dbus-x11
         wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
         mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
         sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
